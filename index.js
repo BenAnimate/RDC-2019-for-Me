97961cbec5c59058662e9fb8fb9abe7e8d0c0f97
1630228 <nastyahmede@gmail.com>
2019-05-20 01:07:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(666)
+const http=require('http');
+const request=require('request');
+const PORT=process.env.PORT||5000;
+var headers={Cookie:'_t='+process.env.t};
+
+function xxx(){
+	request.get({url:'https://devforum.roblox.com/c/roblox-developer-conference/l/latest.json',headers:headers},(e,r,b)=>{
+		console.log(JSON.parse(b).topic_list.topics);
+	});
+}
+
+const server=http.createServer((req,res)=>{
+	res.statusCode=200;
+	res.setHeader('Content-Type','text/plain');
+	res.end('The server should be checking.');
+});
+server.listen(PORT,()=>{
+	console.log(`Server running on ${PORT}.`);
+});
+
+xxx();
+setInterval(()=>{
+	request.get('https://devforum-autom8er.herokuapp.com/');
+  xxx();
+},69000);
