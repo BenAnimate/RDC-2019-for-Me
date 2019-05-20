ca5903f81f10c2775fbd320310b0844227260ba2
1630228 <nastyahmede@gmail.com>
2019-05-20 01:43:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const http=require('http');
 const request=require('request');
 const PORT=process.env.PORT||5000;
 var headers={Cookie:'_t='+process.env.t};
+var t='Count me in; I\'ve made a Heroku application just to ensure I secure a spot.'
 
 function xxx(){
 	request.get({url:'https://devforum.roblox.com/c/roblox-developer-conference/l/latest.json',headers:headers},(e,r,b)=>{
-		console.log(JSON.parse(b).topic_list.topics);
+		var topics=JSON.parse(b).topic_list.topics;
+		request.get({url:'https://devforum.roblox.com/user_actions.json?username=visualplugin',headers:headers},(e,r,b)=>{
+			var actions=JSON.parse(b).user_actions.map(t=>{return t.excerpt});
+			console.log(actions);
+		});
 	});
 }
 
